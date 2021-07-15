ef82ac5d00bce5af3612f3018c88a3ffd5ed879e
223yu <223yuta.jp@gmail.com>
2021-07-15 03:02:31 +0000

    #2 Add controller and action

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,18 @@
 class Public::CustomersController < ApplicationController
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def quit_confirm
+  end
+
+  def quit
+  end
+  
 end
---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,21 @@
+class Public::OrdersController < ApplicationController
+
+  def information
+  end
+
+  def purchase
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def thankyou
+  end
+
+end
