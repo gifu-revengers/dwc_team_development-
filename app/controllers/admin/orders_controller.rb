06e963ea27a9c199097f4128f0a462edbc53c711
223yu <223yuta.jp@gmail.com>
2021-07-16 06:24:28 +0000

    #53 fix order detail show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -17,9 +17,9 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order.status = params[:order_status]
     @order.save
+    @order_details = OrderDetail.where(order_id: @order.id)
     #注文詳細の製作ステータスを更新
     if @order.status == "入金確認"
-      @order_details = OrderDetail.where(order_id: @order.id)
       @order_details.each do |order_detail|
         order_detail.status = "製作待ち"
         order_detail.save
