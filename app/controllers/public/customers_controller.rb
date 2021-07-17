57b9b4e34d02313b9ee86c64c68ded7d273efae2
223yu <223yuta.jp@gmail.com>
2021-07-17 07:44:10 +0000

    #24 make customer edit view

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,13 +1,21 @@
 class Public::CustomersController < ApplicationController
 
   def show
-    @customer = Customer.find(current_customer.id)
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice]="会員情報を更新しました"
+      redirect_to customers_path
+    else
+      render "edit"
+    end
   end
 
   def quit_confirm
@@ -15,5 +23,11 @@ class Public::CustomersController < ApplicationController
 
   def quit
   end
+
+  private
+
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :is_deleted)
+    end
 
 end
