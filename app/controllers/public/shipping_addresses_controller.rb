7c5ee6b54b8b711386768aa53007ad98ff4e0770
EC2 Default User <ec2-user@ip-172-31-33-156.ec2.internal>
2021-07-17 08:52:04 +0000

    create shipping_address index and show page #26 #27

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,18 +1,36 @@
 class Public::ShippingAddressesController < ApplicationController
 
   def index
+    @shipping_address = ShippingAddress.new
+    @shipping_addresses = ShippingAddress.all
   end
 
   def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    @shipping_address.customer_id = current_user.id
+    @shipping_address.save
+    redirect_to request.referer
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
   end
 
   def update
+    @shipping_address = ShippingAddress(params[:id])
+    @shipping_address.update(shipping_address_params)
+    redirect_to shipping_addresses_path
   end
 
   def destroy
+    ShippingAddress.find(params[:id]).destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def shipping_address_params
+    params.require(:shipping_address).permit(:postcode,:address,:address_name)
   end
 
 end
