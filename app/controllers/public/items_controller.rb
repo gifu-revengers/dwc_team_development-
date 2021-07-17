0cc6ae575a89b93f6403317e9fe46fa81bb8555d
EC2 Default User <ec2-user@ip-172-31-33-156.ec2.internal>
2021-07-17 08:44:15 +0000

    create item_show and index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,19 @@
 class Public::ItemsController < ApplicationController
 
   def index
+    @genres = Genre.all
+    @genre = params[:sort]
+    if @genre
+      @items = Item.where(genre_id: @genre).page(params[:page]).per(10)
+    else
+      @items = Item.page(params[:page]).per(10)
+    end
   end
 
   def show
+    @genres = Genre.all
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
 
 end
