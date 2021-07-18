020b55529a0d35b4eac40a2206a64a132b207f83
Taniguchi1204 <tatsuya19951204@gmail.com>
2021-07-18 11:06:15 +0000

    search機能追記

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,9 +2,14 @@ class Public::ItemsController < ApplicationController
 
   def index
     @genres = Genre.all
-    @genre = params[:sort]
-    if @genre
-      @items = Item.where(genre_id: @genre).page(params[:page]).per(10)
+    search = params[:search_word]
+    genre = params[:genre]
+    if search
+      @items = Item.where(['name like(?)', "%#{search}%"]).page(params[:page]).per(10)
+      @title = search
+    elsif genre
+      @items = Item.where(genre_id: genre).page(params[:page]).per(10)
+      @title = Genre.find(genre).name
     else
       @items = Item.page(params[:page]).per(10)
     end
