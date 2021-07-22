2905d0d88c4a2a6841b4ac244ca5398ab10cd25b
223yu <223yuta.jp@gmail.com>
2021-07-22 12:40:37 +0000

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 
 //= require rails-ujs
 //= require activestorage
+//= require jquery3
 //= require turbolinks
 //= require_tree .
 
@@ -24,7 +24,7 @@ $(document).on('turbolinks:load',function(){
   $(".fadeInDown").hide().fadeIn(4000);
 });
 
-$(function () {
+$(document).on('turbolinks:load',function () {
 	$('#zip').jpostal({
 		postcode : [
 			'#zip'
